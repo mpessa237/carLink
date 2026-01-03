d3d6ebfea2bf9812306d29acf56c138b5bdc3617
mpessa237 <hervempessa7@gmail.com>
2026-01-03 08:11:32 +0100

    Fist commit

---
+++ src/main/java/com/herve/carLink/CarLinkApplication.java
@@ -0,0 +1,13 @@
+package com.herve.carLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLinkApplication.class, args);
+	}
+
+}
